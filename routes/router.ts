42c494e02296cfbf871c4ea11416185ac4c3f8a4
Jimmy Rodriguez <jimmy_sidney@hotmail.es>
2024-07-20 23:12:12 -0600

    update packages and routes

--- routes/router.ts
+++ routes/router.ts
@@ -7,10 +7,26 @@ router.get('/mensajes',(req: Request, res: Response) => {
 
     res.json({
         ok: true,
-        mensaje: 'Todo esta bien - POST!!'
+        mensaje: 'Todo esta bien - GET!!'
     })
 
 })
+
+router.post('/mensajes',(req: Request, res: Response) => {
+
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    const id = req.params.id;
+
+    res.json({
+        ok: true,
+        mensaje: 'Todo esta bien - POST!!', 
+        cuerpo: cuerpo,
+        de: de,
+        id: id,
+    })
+
+});
 
 router.post('/mensajes/:id',(req: Request, res: Response) => {
 
@@ -20,7 +36,7 @@ router.post('/mensajes/:id',(req: Request, res: Response) => {
 
     res.json({
         ok: true,
-        mensaje: 'Todo esta bien - GET!!', 
+        mensaje: 'Todo esta bien - POST!!', 
         cuerpo: cuerpo,
         de: de,
         id: id,
